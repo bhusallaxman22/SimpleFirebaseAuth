bfd172b0ff7b77a31764e62fb72cc7d116b32ee4
bhusallaxman22 <laxmanbhusal612@gmail.com>
2020-01-12 22:10:56 +0545

    Updates

---
+++ precache-manifest.e36645bfc93f433a6e28020c58f7a619.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1249b19e7657581de3b0132ce5ec6639",
+    "url": "/TodoAuth/index.html"
+  },
+  {
+    "revision": "123ee98f4113d6dbca1a",
+    "url": "/TodoAuth/static/css/main.84dd4a6b.chunk.css"
+  },
+  {
+    "revision": "43e99f8a7b4a2577918b",
+    "url": "/TodoAuth/static/js/2.2172c08f.chunk.js"
+  },
+  {
+    "revision": "123ee98f4113d6dbca1a",
+    "url": "/TodoAuth/static/js/main.add149c8.chunk.js"
+  },
+  {
+    "revision": "177135d172922fe4d357",
+    "url": "/TodoAuth/static/js/runtime-main.64854bf8.js"
+  }
+]);
